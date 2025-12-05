b30392f873d5ddbdda95237f6e6d304cc516ef5e
APPLE <vonenzell@gmail.com>
2025-12-05 10:53:16 +0300

    javascript scope lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution in this file!
+let burgers = [ `Hamburger`, `Cheeseburger`, ];
+let featureDrink = `Strawberry Milkshake` ;
+
+function addBurger() {
+    let newBurger = `Flatburger` ;
+    burgers.push(newBurger);
+}
+
+if (true) {
+    let anotherNewBurger = `Maple Bacon Burger` ;
+    burgers.push(anotherNewBurger);
+}
+
+function changeFeatureDrink() {
+    featureDrink = `JavaShake` ;
+}
+
+console.log(`Initial Burgers:`, burgers);
+console.log(`Initial Feature Drink:`, featureDrink);
+
+addBurger(); 
+changeFeatureDrink();
+
+console.log(`After calls - Burgers:`, burgers);
+console.log(`After calls - Feature Drink:`, featureDrink);
